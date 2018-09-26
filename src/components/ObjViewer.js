b7e0273e4f9ebfd3362f61aeee9a8c8484c8c325
Chris Maltby <chris@feed.xyz>
2018-09-26 16:20:49 +0100

    Deallocate aframe on exit

--- src/components/ObjViewer.js
+++ src/components/ObjViewer.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { Entity, Scene } from 'aframe-react';
 try {
     require("aframe");
+    window.AFRAME.registerComponent('deallocate', {
+        remove: function () {
+            window.THREE.Cache.clear();
+            this.el.renderer.forceContextLoss();
+        },
+    });
 } catch (e) {
     // AFRAME CANT IMPORT - This happens during gatsby build, it's fine
 }
@@ -10,7 +16,7 @@ class VRScene extends React.Component {
     render() {
         const { src } = this.props;
         return (
-            <Scene embedded vr-mode-ui="enabled: false">
+            <Scene embedded vr-mode-ui="enabled: false" deallocate>
                 <Entity light={{ type: 'point' }} />
                 <Entity obj-model={{ obj: src }} material={{ color: 'blue' }} position={{ x: 0, y: 1.5, z: -5 }} />
             </Scene>
