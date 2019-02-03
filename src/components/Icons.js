7351e02ef75948593ba3b11fb4c888a2a0b7eea8
Chris Maltby <chris.maltby@gmail.com>
2019-02-03 17:45:54 +0000

    Added video play/pause buttons

---
+++ src/components/Icons.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+export const PlayIcon = () => (
+  <svg
+    version="1.1"
+    id="Layer_1"
+    x="0px"
+    y="0px"
+    viewBox="0 0 30 30"
+    style={{ enableBackground: "new 0 0 30 30" }}
+  >
+    <g>
+      <rect
+        x="0.5"
+        y="0.5"
+        class="st0"
+        width="29"
+        height="29"
+        style={{
+          fill: "#FFFFFF"
+        }}
+      />
+      <path
+        class="st1"
+        d="M29,1v28H1V1H29 M30,0H0v30h30V0L30,0z"
+        style={{
+          fill: "#1D1D1B"
+        }}
+      />
+    </g>
+    <polyline
+      class="st2"
+      points="6.3,6.3 23.7,15 6.3,23.7"
+      style={{
+        fill: "none",
+        stroke: "#1D1D1B",
+        strokeMiterlimit: 10
+      }}
+    />
+  </svg>
+);
+
+export const PauseIcon = () => (
+  <svg
+    version="1.1"
+    id="Layer_1"
+    x="0px"
+    y="0px"
+    viewBox="0 0 30 30"
+    style={{ enableBackground: "new 0 0 30 30" }}
+  >
+    <g>
+      <rect
+        x="0.5"
+        y="0.5"
+        class="st0"
+        width="29"
+        height="29"
+        style={{ fill: "#FFFFFF" }}
+      />
+      <path
+        class="st1"
+        d="M29,1v28H1V1H29 M30,0H0v30h30V0L30,0z"
+        style={{ fill: "#1D1D1B" }}
+      />
+    </g>
+    <line
+      class="st2"
+      x1="10"
+      y1="23.7"
+      x2="10"
+      y2="6.3"
+      style={{ fill: "none", stroke: "#1D1D1B", strokeMiterlimit: 10 }}
+    />
+    <line
+      class="st2"
+      x1="20"
+      y1="23.7"
+      x2="20"
+      y2="6.3"
+      style={{ fill: "none", stroke: "#1D1D1B", strokeMiterlimit: 10 }}
+    />
+  </svg>
+);
--- src/components/VideoViewer.js
+++ src/components/VideoViewer.js
@@ -1,21 +1,45 @@
 import React, { Component } from "react";
 import cx from "classnames";
+import { PlayIcon, PauseIcon } from "./Icons";
 import "./VideoViewer.css";
 
 export default class VideoViewer extends Component {
-    render() {
-        const { src, poster, visible } = this.props;
-        return (
-            <video
-                className={cx("VideoViewer", {
-                    "VideoViewer--Hidden": !visible
-                })}
-                poster={poster}
-                muted
-                autoPlay
-            >
-                <source src={src} type="video/mp4" />
-            </video>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+    this.state = {
+      paused: true
+    };
+  }
+
+  onTogglePlay = () => {
+    const video = this.videoRef.current;
+    video.paused ? video.play() : video.pause();
+    this.setState({
+      paused: video.paused
+    });
+  };
+
+  render() {
+    const { src, poster, visible } = this.props;
+    const { paused } = this.state;
+    return (
+      <div
+        className={cx("VideoViewer", {
+          "VideoViewer--Hidden": !visible,
+          "VideoViewer--Playing": !paused
+        })}
+      >
+        <video poster={poster} ref={this.videoRef} muted={!visible}>
+          <source src={src} type="video/mp4" />
+        </video>
+
+        <div className="VideoViewer__Buttons" onClick={this.onTogglePlay}>
+          <div className="VideoViewer__Button">
+            {paused ? <PlayIcon /> : <PauseIcon />}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
